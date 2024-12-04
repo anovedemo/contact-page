1eed8ba72c7bfa80c096d0d6ab99b7e801f66c43
ezrabuenkanove <ezra.buenk@anove.agency>
2024-12-04 16:48:05 +0100

    Mobile responsive

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import ContactCard from "../components/ContactCard";
 import Footer from "../components/Footer";
 import HeaderTransparent from "../components/HeaderTransparent";
@@ -7,15 +6,17 @@ import 'animate.css';
 function Contact() {
     return (
         <div className="w-full flex flex-col items-center">
-            <div className="w-full bg-gradient-to-r from-cornell-red-700 md:from-0% from-[-30%] to-mid-blue-700 md:to-100% to-130% h-[30vh] md:h-[450px] flex items-center flex-col">
+            <div className="w-full bg-gradient-to-r from-cornell-red-700 md:from-0% from-[-30%] to-mid-blue-700 md:to-100% to-130% h-[50vh] md:h-[600px] flex items-center flex-col">
                 <HeaderTransparent />
-                <div class="border-t border-white w-[95vw] md:w-2/3 hidden md:flex"></div>
-                <div className="flex items-center h-full md:w-2/3">
-                    <h1 className="text-4xl md:text-6xl font-medium unbounded text-white text-center mb-10 md:mb-0 animate__animated animate__fadeInLeft">Contact Us</h1>
+                <div class="border-t mt-1 border-white w-[95vw] md:w-2/3"></div>
+                <div className="flex flex-col justify-center items-start h-full w-[95vw] md:w-2/3 gap-5 mb-20">
+                    <h1 className="text-4xl md:text-6xl font-medium unbounded text-white text-center animate__animated animate__fadeInLeft mt-10">Contact Us</h1>
+                    <p className="text-white font-medium text-lg md:max-w-[25vw]">We’d love to show you how we can simplify and automate your compliance process.
+                    <br></br>Get in touch with us through any of the following options!</p>
                 </div>
             </div>
             <div className="w-[90vw] lg:w-2/3 flex flex-col items-center mb-36">
-                <h1 className="text-3xl text-center md:text-4xl font-semibold mt-20 unbounded">Get in contact</h1>
+                <h1 className="text-3xl text-center md:text-4xl font-semibold mt-20 unbounded">Let's Get In Touch</h1>
                 <div class="border-t-2 border-gray-300 w-full my-16"></div>
                 <div className="flex flex-wrap gap-10 md:gap-7 justify-center lg:justify-between w-full">
                     <ContactCard
